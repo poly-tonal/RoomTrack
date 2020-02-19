2f7cb283e13754185bb3991bf55e3df8193d4c86
ellie <ellieepprice@gmail.com>
2020-02-19 18:07:08 +0000

    first Commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,52 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofSetWindowTitle("roomTrack");
+	ofSetBackgroundColor(0);
+	osc.setup(HOST, PORT);
+	grab.setDeviceID(1);
+	grab.setup(1024, 768);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	//ofxOscMessage message;
+	grab.update();
+	if (grab.isFrameNew()) {
+		//cont.setTargetColor(color);
+		cont.setThreshold(60);
+		cont.findContours(grab);
+		//message.addBoolArg(playing);
+		//oscx = ofMap(cont.getCentroid(0).x, 0, ofGetWidth(), 0.0f, 200.0f, true);
+		//oscy = ofMap(cont.getCentroid(0).y, 0, ofGetHeight(), 1.0f, 0.0f, true);
+		//message.addFloatArg(oscx);
+		//message.addFloatArg(oscy);
+	}
+	//osc.sendMessage(message, false);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	ofSetColor(255);
+	grab.draw(0, 0);
+	cont.draw();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+	if (isdigit(key)) {
+		num = ofMap(key, 48, 57, 0, 9);
+		if (num != cam) {
+			cam = num;
+			changeCam(cam);
+		}
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::changeCam(int cam) {
+	grab.close();
+	grab.setDeviceID(cam);
+	grab.initGrabber(1024, 768);
+}
---
+++ src/ofApp.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOsc.h"
+#include "ofxCv.h"
+
+#define PORT 9001
+#define HOST "localhost"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		//void keyReleased(int key);
+		//void mouseMoved(int x, int y );
+		//void mouseDragged(int x, int y, int button);
+		//void mousePressed(int x, int y, int button);
+		//void mouseReleased(int x, int y, int button);
+		//void mouseEntered(int x, int y);
+		//void mouseExited(int x, int y);
+		//void windowResized(int w, int h);
+		//void dragEvent(ofDragInfo dragInfo);
+		//void gotMessage(ofMessage msg);
+		void changeCam(int camera);
+		
+		ofxOscSender osc;
+		float oscx = 0.0f;
+		float oscy = 0.0f;
+		bool playing = false;
+		bool switched = false;
+		ofVideoGrabber grab;
+		ofxCv::ContourFinder cont;
+		//ofColor color;
+		int num = 1;
+		int cam = 0;
+};
